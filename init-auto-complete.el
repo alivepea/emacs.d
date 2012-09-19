b51c295fac7e16b7b4ab4b90319a70c81e375541
watson <linyunxian@gmail.com>
2012-09-19 17:34:04 +0800

    Disable tab/C-n/C-p auto-complete

--- init-auto-complete.el
+++ init-auto-complete.el
@@ -3,13 +3,13 @@
 (global-auto-complete-mode t)
 (setq ac-auto-start 4)
 (setq ac-dwim nil) ; To get pop-ups with docs even if a word is uniquely completed
-(define-key ac-completing-map (kbd "C-n") 'ac-next)
-(define-key ac-completing-map (kbd "C-p") 'ac-previous)
+;; (define-key ac-completing-map (kbd "C-n") 'ac-next)
+;; (define-key ac-completing-map (kbd "C-p") 'ac-previous)
 
 ;;----------------------------------------------------------------------------
 ;; Use Emacs' built-in TAB completion hooks to trigger AC (Emacs >= 23.2)
 ;;----------------------------------------------------------------------------
-(setq tab-always-indent 'complete)  ;; use 'complete when auto-complete is disabled
+;; (setq tab-always-indent 'complete)  ;; use 'complete when auto-complete is disabled
 (add-to-list 'completion-styles 'initials t)
 
 ;; hook AC into completion-at-point
