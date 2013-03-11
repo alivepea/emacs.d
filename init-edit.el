23fbd8c6e62033dce027546a54433616920d8f6a
watson <linyunxian@gmail.com>
2013-03-11 09:44:31 +0800

    "fill-paragraph" support ZH

--- init-edit.el
+++ init-edit.el
@@ -1,13 +1,17 @@
-
 (setq-default major-mode 'text-mode)
-
 (electric-pair-mode t)
+(setq kill-read-only-ok t)
+(setq kill-do-not-save-duplicates t)
 
 (global-set-key (kbd "C-w") 'backward-kill-word)
 (global-set-key (kbd "C-x C-w") 'kill-region)
 
-(setq kill-read-only-ok t)
-(setq kill-do-not-save-duplicates t)
+;; [M-q] fill-paragrah
+(setq default-fill-column 70)
+;;设置 sentence-end 可以识别中文标点。不用在 fill 时在句号后插入两个空
+;;格。
+(setq sentence-end "\\([。！？]\\|……\\|[.?!][]\"')}]*\\($\\|[ \t]\\)\\)[ \t\n]*")
+(setq sentence-end-double-space nil)
 
 (defun copy-above-to-char (arg char)
   "Copy all characters from the previous line beginning with the
