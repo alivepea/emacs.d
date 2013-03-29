31bf6664ee98682d3b77bfae31a4e9fde6f8f267
watson <linyunxian@gmail.com>
2013-03-29 11:13:49 +0800

    customize term-color-blue

--- custom.el
+++ custom.el
@@ -3,4 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(term-color-blue ((t (:background "blue2" :foreground "RoyalBlue1"))))
  '(trailing-whitespace ((t (:background "dim gray")))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
