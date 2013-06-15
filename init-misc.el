f8bdecdb4931b0a45e43c06f428c8d1cf57f4a6f
watson <linyunxian@gmail.com>
2013-06-16 03:05:25 +0800

    <M-;> comment/uncomment current line when empty active region

--- init-misc.el
+++ init-misc.el
@@ -40,7 +40,22 @@
 (define-key occur-mode-map "n" 'occur-next)
 (define-key occur-mode-map "p" 'occur-prev)
 
+
+(defun qiang-comment-dwim-line (&optional arg)
+  "Replacement for the comment-dwim command.
+If no region is selected and current line is not blank and we are not at the end of the line,
+then comment current line.
+Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
+  (interactive "*P")
+  (comment-normalize-vars)
+  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+      (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    (comment-dwim arg)))
+(global-set-key "\M-;" 'qiang-comment-dwim-line)
+
+
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
+
 
 (provide 'init-misc)
