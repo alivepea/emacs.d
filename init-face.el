9375c97e61ed76f039c5b683371fd132c16d7c98
watson <linyunxian@gmail.com>
2013-06-16 02:50:13 +0800

    * add Han&&English font setting

--- init-face.el
+++ init-face.el
@@ -36,7 +36,23 @@
 (setq x-stretch-cursor t)
 (setq-default show-trailing-whitespace t)
 
-;; (global-set-key (kbd "<f1>") 'menu-bar-mode)
+;; Setting English Font
+(set-face-attribute
+  'default nil :font "Dejavu Sans Mono 12")
+
+;; Chinese Font
+(dolist (charset '(kana han symbol cjk-misc bopomofo))
+    (set-fontset-font (frame-parameter nil 'font)
+                      charset
+                      (font-spec :family "WenQuanYi Micro Hei" :size 16)))
+
+;; For Linux
+(global-set-key (kbd "<C-mouse-4>") 'text-scale-increase)
+(global-set-key (kbd "<C-mouse-5>") 'text-scale-decrease)
+
+;; For Windows
+(global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
+(global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
 
 
 (provide 'init-face)
