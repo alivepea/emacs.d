cccde1bc78c8b8e51abee0f083483e4e72ebf15d
watson <watson_liu@htc.com>
2013-06-26 11:54:37 +0800

    Add <C-S-tab> invert cycle window

--- init-window.el
+++ init-window.el
@@ -1,4 +1,7 @@
 (global-set-key (kbd "C-<tab>") 'other-window)
+(global-set-key (kbd "<C-S-iso-lefttab>") (lambda ()
+				    (interactive)
+				    (other-window -1)))
 
 (windmove-default-keybindings 'shift)
 (setq windmove-wrap-around t)
