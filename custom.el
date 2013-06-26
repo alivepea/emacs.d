37fddd1c79a8db9973be4544eaabc1fd7deb4e3d
watson <watson_liu@htc.com>
2013-06-26 10:13:49 +0800

    gtags case enable

--- custom.el
+++ custom.el
@@ -11,4 +11,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(git--timer-sec 2.0))
+ '(git--timer-sec 2.0)
+ '(gtags-ignore-case nil))
